da3b57e257d715f29ee9ffaeae6cae482fb2b833
Jorge Ortiz <jorge.ortiz@gmail.com>
2010-10-12 17:49:22 -0400

    Compile under Scala 2.8.0

---
+++ project/build/GrabbyHandsProject.scala
@@ -0,0 +1,9 @@
+import sbt._
+
+class ConfiggyProject(info: ProjectInfo) extends DefaultProject(info) {
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3" % "compile"
+  val junit = "junit" % "junit" % "4.7" % "test"
+
+  override def compileOptions = super.compileOptions ++ Seq(CompileOption("-Xmigration"))
+}
